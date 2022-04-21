bee03012f606da3e2643dcebe89fbcb9d4cda81d
Victor <victorswaroop2002@gmail.com>
2022-04-21 20:03:37 +0530

    minor changes

--- index.js
+++ index.js
@@ -220,6 +220,7 @@ io.of(/^\/dynamic-\d+$/).on('connection', (socket) => {
     
 });
 
-server.listen(3000, () => {
-    console.log('listening on port 3000...');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`listening on port ${port}...`);
 });
